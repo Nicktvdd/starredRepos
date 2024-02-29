cbe41cf3a46b7310267fe167dff524c8a5c2ad8d
nicktvdd <nicktvdd@gmail.com>
2024-02-29 17:23:31 +0200

    succesfully omitting private and license

--- main.py
+++ main.py
@@ -46,11 +46,20 @@ def create_repos_response(repos):
         {
             "name": repo["name"],
             "url": repo["html_url"],
-            "license": repo["license"]["name"] if repo["license"] else None,
+            "license": repo["license"]["name"] if repo.get("license") else None,
+            "description": repo.get("description"),
+            "topics": repo.get("topics")
+        }
+        if repo.get("license")
+        else
+        {
+            "name": repo["name"],
+            "url": repo["html_url"],
             "description": repo.get("description"),
             "topics": repo.get("topics")
         }
         for repo in repos
+        if not repo.get("private")
     ]
     return {"number_of_repos": len(repos_info), "repos": repos_info}
 
